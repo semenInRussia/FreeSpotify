6394d96f1b712713292a28aa061b383ef75c3418
semenInRussia <semenprofis999@gmail.com>
2020-10-04 15:29:31 +0300

    update urls.py

--- backend/FreeSpotifyBackEnd/urls.py
+++ backend/FreeSpotifyBackEnd/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tracks/', include('tracks.urls')),
+    path('api/tracks/', include('tracks.urls')),
 ]

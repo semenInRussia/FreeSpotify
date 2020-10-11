c0ef32311abfdb5d6d6431d79f3924b0bacc964f
semenInRussia <semenprofis999@gmail.com>
2020-10-11 13:29:58 +0300

    add set album img in get_tracks_top()

--- backend/buisness_logic/publicFeatures.py
+++ backend/buisness_logic/publicFeatures.py
@@ -1,6 +1,6 @@
 from backend.buisness_logic.SpotifyWebAPI.features import Spotify
 from backend.buisness_logic.core.exceptions import NotFoundAlbumException, NotFoundArtistException
-from backend.buisness_logic.rocknationAPI import get_link_on_album, get_link_on_artist
+from backend.buisness_logic.rocknationAPI import get_link_on_album, get_link_on_artist, get_link_on_album_img
 from backend.buisness_logic.spotifyPythonAPI import get_top_music_info, get_top_music_info_by_approximate_artist_title
 
 
@@ -9,27 +9,39 @@ def get_tracks_top(artist_name, spotify: Spotify) -> list:
 
     cash = {
         'links_on_album': {},
+        'links_on_album_img': {}
     }
 
-    link_on_artist = ""
+    link_on_artist = get_link_on_artist(artist_name)
 
     for track in tracks_info:
-
-        artist_name = track["artist_name"]
         album_name = track['album_name']
 
-        # get link on artist
-        if link_on_artist == "":
-            link_on_artist = get_link_on_artist(artist_name)
-
         # get link on album
-        link_on_album = cash['links_on_album'].get(album_name)
-        if not link_on_album:
-            link_on_album = get_link_on_album(artist_name, album_name, raise_exception=False)
-            cash['links_on_album'][album_name] = link_on_album
+        link_on_album = _get_value_from_cash(cash['links_on_album'], key=album_name,
+                                             get_default_value=lambda key: get_link_on_album(artist_name,
+                                                                                             album_name,
+                                                                                             raise_exception=False))
+        link_on_album_img = _get_value_from_cash(cash['links_on_album'], key=album_name,
+                                                 get_default_value=lambda key: get_link_on_album_img(
+                                                     link_on_album=link_on_album))
 
         # update data
         track["album_link"] = link_on_album
         track["artist_link"] = link_on_artist
+        track["album_img_link"] = link_on_album_img
 
     return tracks_info
+
+
+def _get_value_from_cash(cash: dict, key: str, get_default_value):
+    """
+    Get value from cash
+    :param cash:
+    :param key: key from cash
+    :param get_default_value: is function (key) -> default value
+    :return:
+    """
+    if not (key in cash):
+        cash[key] = get_default_value(key)
+    return cash[key]
